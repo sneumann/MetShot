a32b3b50e1d5cacf6d40ddd524ccf1c2a55a7820
sneumann <sneumann@88837528-0963-4635-b47b-91f4607bead6>
2011-07-22 12:14:11 +0000

    Fixed unit test script
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/R/trunk/MetShot@2615 88837528-0963-4635-b47b-91f4607bead6

--- tests/doRUnit.R
+++ tests/doRUnit.R
@@ -3,7 +3,7 @@ if(require("RUnit", quietly=TRUE)) {
 
   ## --- Setup ---
 
-  pkg <- "mzR" # <-- Change to package name!
+  pkg <- "MetShot" # <-- Change to package name!
   if(Sys.getenv("RCMDCHECK") == "FALSE") {
     ## Path to unit tests for standalone running under Makefile (not R CMD check)
     ## PKG/tests/../inst/unitTests
